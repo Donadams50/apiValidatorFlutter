d6574bf6c70082371da2c72319ddc6dbe05e9474
unknown <sumbomatic@gmail.com>
2021-01-28 20:41:31 +0100

    update

--- app/validator/validator.controller.js
+++ app/validator/validator.controller.js
@@ -26,22 +26,53 @@ exports.getMyProfile = async (req, res) => {
 
 //validate rule 
 exports.validateRule = async (req, res) => {
-    try{
-           const findmyprofile = await myprofilemodel.myProfile() 
-            res.status(200).send(
-                {
-                    message: "My Rule-Validation API",
-                    status: "success",
-                    data: findmyprofile
-                })
-              
-       }catch(err){
-             console.log(err)
-            res.status(500).send(
-             {
-                message: "My Rule-Validation API",
-                status: "error",
-                data: null
-            })
-       }
+    if (!req.body){
+        res.status(400).send({message:"Content cannot be empty"});
+    }
+console.log(req.body)
+
+    const {  officeTitle , officeTitleBranch} = req.body;
+  
+    if (  officeTitle  ){
+        if ( officeTitle===""    ){
+            res.status(400).send({
+                message:"Incorrect entry format"
+            });
+    }else{  
+              const combinedOfficeBranch = ''+officeTitle+' '+officeTitleBranch+''
+            const office = new Offices({
+            
+                officeTitleBranch: officeTitleBranch,
+                officeTitle: officeTitle,
+                isAssigned: false,
+                userInOffice: "" ,
+                userNameInOffice: "",
+                combinedOfficeBranch: combinedOfficeBranch         
+
+                
+              });
+        
+            try{   
+                const isOfficeExist = await Offices.findOne({combinedOfficeBranch: combinedOfficeBranch} )
+
+                 if(isOfficeExist){
+                    res.status(400).send({
+                        message:"Office already exist"
+                    });
+                 }else{
+
+                            const saveOffice = await  office.save()
+                            console.log(saveOffice)                
+                            res.status(201).send({message:"Office created"})
+                        }
+            }catch(err){
+                console.log(err)
+                res.status(500).send({message:"Error while creating office "})
+            }
+        }
+    }else{
+        res.status(400).send({
+            message:"Incorrect entry format"
+        });
+    }
 };
