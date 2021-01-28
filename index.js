3c0d72abc8cc7daf928d56bfda8963a0c4de9669
unknown <sumbomatic@gmail.com>
2021-01-28 12:09:17 +0100

    update

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+const cors = require("cors");
+app.use(cors()); 
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+ require("./app/product/product.routes.js")(app)
+ require("./app/members/members.routes.js")(app)
+ require("./app/cart/cart.routes.js")(app)
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to api validator  application." });
+  });
+
+
+// Connect to port
+const port = process.env.PORT || 4000     
+
+app.listen(port, ()=> console.log(`listening on port ${port}...`)); 
