7be3b250b18396e0b3be34c6862e779cc1c95564
unknown <sumbomatic@gmail.com>
2021-01-29 17:14:14 +0100

    update

--- app/validator/validator.controller.js
+++ app/validator/validator.controller.js
@@ -35,37 +35,16 @@ if (!req.body){
   
     if (  rule  ){
         if (   data  ){
-        if ( officeTitle===""    ){
+        if ( rule==={}    ){
             res.status(400).send({
                 message:"Incorrect entry format"
             });
-    }else{  
-              const combinedOfficeBranch = ''+officeTitle+' '+officeTitleBranch+''
-            const office = new Offices({
-            
-                officeTitleBranch: officeTitleBranch,
-                officeTitle: officeTitle,
-                isAssigned: false,
-                userInOffice: "" ,
-                userNameInOffice: "",
-                combinedOfficeBranch: combinedOfficeBranch         
-
-                
-              });
+     }else{  
+              
         
             try{   
-                const isOfficeExist = await Offices.findOne({combinedOfficeBranch: combinedOfficeBranch} )
-
-                 if(isOfficeExist){
-                    res.status(400).send({
-                        message:"Office already exist"
-                    });
-                 }else{
-
-                            const saveOffice = await  office.save()
-                            console.log(saveOffice)                
-                            res.status(201).send({message:"Office created"})
-                        }
+                
+                     
             }catch(err){
                 console.log(err)
                 res.status(500).send({message:"Error while creating office "})
--- app/validator/validator.model.js
+++ app/validator/validator.model.js
@@ -11,3 +11,77 @@ const myProfile = {
 exports.myProfile=()=> {
     return myProfile;
   }
+
+  exports.isRuleValidated= (req, res, next)=> { 
+    const {  rule } = req.body;
+  
+        if ( rule) {
+            if (  typeof rule === 'object' && rule !== null    ){
+                if (  rule.field && rule.condtion && rule.condition_value   ){
+                    if (  rule.field === " " || rule.condtion === " " || rule.condition_value === ""   ){
+
+                        res.status(400).send({          
+                            message: "The required field cannot be empty.",
+                            status: "error",
+                            data: null
+                          
+                    });
+                        
+                        
+                 }else{
+                    next();
+                 }
+                
+                    
+                    
+             }else{ 
+                
+                res.status(400).send({          
+                    message: "One of the required field in the rule object is not available.",
+                    status: "error",
+                    data: null
+                  
+            });
+    
+             } 
+
+                next();
+                
+         }else{ 
+            
+            res.status(400).send({          
+                message: "rule should be an object.",
+                status: "error",
+                data: null
+              
+        });
+
+         }        
+          
+        }else{
+            res.status(400).send({          
+                message: "rule field is required.",
+                status: "error",
+                data: null
+              
+        });
+          
+        }
+    
+  }
+
+
+  exports.isDataValidated= (req, res, next)=> { 
+    const token = req.headers.authorization || req.params.token;
+  
+        if (req.user.roleId === 10) {
+         console.log(req.user.role) 
+          next();
+          
+        }else{
+          console.log(req.user.role) 
+          res.status(401).json({ status: 401, error: 'Unauthorized to access this resource' });
+          
+        }
+    
+  }
